acfa23f73e45d024da5486dd47c3a484efdbd24c
Tomas Knejp <tomas.knejp@cncenter.cz>
2023-12-24 16:55:52 +0100

    jenkins

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,31 +2,30 @@ pipeline {
     agent {
         docker {
             image 'node:18-slim'
-            args '-u root:root' // Run as root if needed, or remove if not
+            args '-u root:root'
         }
     }
 
+    environment {
+        // Define environment variables if needed, such as API endpoints or keys
+    }
+
     stages {
+        stage('Install Dependencies') {
+            steps {
+                sh 'yarn install'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'yarn build'
+            }
+        }
         stage('Deploy Production') {
             when {
-                branch 'main' // Runs only when the branch is 'main'
+                branch 'main' 
             }
             steps {
                 script {
                     sh 'npx next telemetry disable'
-                    // Replace the following lines with your actual deployment script
-                    // sh 'npm install'
-                    // sh 'npm run build'
-                    // Add your deployment commands here
-                }
-            }
-        }
-    }
-
-    post {
-        always {
-            // Clean up the workspace to not leave anything behind
-            cleanWs()
-        }
-    }
-}
+                    
